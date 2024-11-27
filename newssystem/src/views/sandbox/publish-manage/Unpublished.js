dd3fbfefd0ba3fe736f72f2b258539a78b6b7db9
Vicky-Dai <dai.ke1@northeastern.edu>
2024-11-26 21:12:00 -0800

    1126

--- newssystem/src/views/sandbox/publish-manage/Unpublished.js
+++ newssystem/src/views/sandbox/publish-manage/Unpublished.js
@@ -1,24 +1,20 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import NewsPublish from '../../../components/publish-manage/NewsPublished.js'
+import usePublish from '../../../components/publish-manage/usePublish.js'
+import { Button } from 'antd'
 
 
 
 export default function Unpublished() {
-  const {username} = JSON.parse(localStorage.getItem("token"))
-  const [dataSource, setdataSource] = useState([])
-
-  useEffect(()=>{
-    axios(`/news?author=${username}&publishState=1&_embed=category`).then(res=>{
-      console.log(res.data)
-      setdataSource(res.data)
-    })
-  },[username])
+  const {dataSource, handlePublish} = usePublish(1) /* 1是待发布的 */
   
-
   return (
     <div>
-      <NewsPublish dataSource = {dataSource}> </NewsPublish>
+      <NewsPublish dataSource = {dataSource} button={(id)=><Button type ="primary"
+       onClick={()=>{handlePublish(id)}}>
+        发布
+      </Button>}> </NewsPublish>
     </div>
   )
 }
